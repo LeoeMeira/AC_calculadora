b39ca94582ffdb265a279a7bdb50924715ea7d50
Leonardo Meira de Lima <91034013+LeoeMeira@users.noreply.github.com>
2022-04-08 19:33:50 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('calc.html')
+
+
+@app.route('/calculadora', methods=['POST'])
+def calcular():
+    valor1 = int(request.form['v1'])
+    valor2 = int(request.form['v2'])
+    operacao = request.form['operacao'].upper()
+    if operacao == 'SOMA':
+        resultado = valor1 + valor2
+        return render_template('resultado.html', resultado=resultado)
+    elif operacao == 'SUBTRACAO':
+        resultado = valor1 - valor2
+        return render_template('resultado.html', resultado=resultado)
+    elif operacao == 'MULTIPLICACAO':
+        resultado = valor1 * valor2
+        return render_template('resultado.html', resultado=resultado)
+    elif operacao == 'DIVISAO':
+        resultado = valor1 / valor2
+        return render_template('resultado.html', resultado=resultado)
+    else:
+        return '<h1> Operação inválida </h1>'
+
+
+app.run(debug=True)
